5816f65ff034c0df304c2f4379686b712a9191f9
Wendy Navichoque <navichoquewendy@gmail.com>
2020-06-08 11:56:00 -0400

    Initial Commit

---
+++ BLM Resources/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BLM Resources
+//
+//  Created by admin on 6/8/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BLM ResourcesTests/BLM_ResourcesTests.swift
@@ -0,0 +1,34 @@
+//
+//  BLM_ResourcesTests.swift
+//  BLM ResourcesTests
+//
+//  Created by admin on 6/8/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import XCTest
+@testable import BLM_Resources
+
+class BLM_ResourcesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
